a4daa550ee664b0dd04b565a54cde4ee303d71ec
Kazumachi124170 <andmichi1998@example.com>
2020-05-15 12:51:32 +0800

    Post page html

---
+++ ser.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 65530
+
+const http = require('http').createServer(app)
+
+
+http.listen(port, () => {
+  console.log(`Server start on port: ${port}`)
+})
+
+
+app.use(express.static(`${__dirname}/dist`))
+
+
